bd9c00beca3e027328ee307081b9bc487499befd
Sonal <sonal@magicbricks.com>
2024-12-27 18:24:55 +0530

    Initial Commit

---
+++ tut/tutApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutApp.swift
+//  tut
+//
+//  Created by Sonal on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct tutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
